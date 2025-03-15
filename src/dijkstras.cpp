f928ebb85a2161589e9801e94731399842af7004
HaichaoLiag <xliang973@gmail.com>
2025-03-15 03:37:45 +0000

    3

--- src/dijkstras.cpp
+++ src/dijkstras.cpp
@@ -50,9 +50,8 @@ void print_path(const vector<int>& path, int total) {
         return;
     }
 
-    for (size_t i = 0; i < path.size(); ++i) {
-        if (i > 0) cout << " ";
-        cout << path[i];
+    for (size_t i = 1; i < path.size(); ++i) {
+        cout << " " << path[i];
     }
     cout << "\nTotal cost is " << total << endl;
 }
